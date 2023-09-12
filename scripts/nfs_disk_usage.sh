10173fe606cd6c47893204b9ba6ef3bbd6014a06
zinza <toilatung52@gmail.com>
2023-09-12 17:56:15 +0700

    Update script to handle du take many time

--- scripts/nfs_disk_usage.sh
+++ scripts/nfs_disk_usage.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-currentPath=$(pwd)
+runningFile=/root/metrics-running.txt
 
-if [ $1 = . ]; then
-  path=$(pwd)
-else
-  path=$1
-fi
-
-# Actions
-array=($(ls -d $path/*/))
-
-# Set up a metric file
-metricFilepPath="$METRICS_FILE_PATH/metrics.txt"
 mkdir -p $METRICS_FILE_PATH
-echo "# HELP nfs_disk_usage_bytes The number of bytes usage" > $metricFilepPath
-echo "# TYPE nfs_disk_usage_bytes gauge" >> $metricFilepPath
 
-for i in ${array[@]}
-do
-  cd $i
-  record="$(du -s)"
-  bytes=$(echo "$record" | awk '{ print substr( $0, 1, length($0)-2 ) }')
+if [ -f $runningFile ]; then
+  exit # File exists, which means prev execution has not completed.
+else
+  if [ $1 = . ]; then
+    path=$(pwd)
+  else
+    path=$1
+  fi
 
-  echo "nfs_disk_usage_bytes{volume=\"$i\"} $bytes" >> $metricFilepPath
-done
+  # Actions
+  array=($(ls -d $path/*/))
 
-cd $currentPath
+  # Set up a metric file
+  echo "# HELP nfs_disk_usage_bytes The number of bytes usage" > $runningFile
+  echo "# TYPE nfs_disk_usage_bytes gauge" >> $runningFile
+
+  for i in ${array[@]}
+  do
+    cd $i
+    record="$(du -s -b)"
+    bytes=$(echo "$record" | awk '{ print substr( $0, 1, length($0)-2 ) }')
+
+    echo "nfs_disk_usage_bytes{volume=\"$i\"} $bytes" >> $runningFile
+  done
+
+  mv $runningFile $METRICS_FILE_PATH/metrics.txt
+fi
