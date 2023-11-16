829990c5bd69640fe54414c68b1c12dbea854a6f
zinza <toilatung52@gmail.com>
2023-11-16 18:02:03 +0700

    Set up new bash script

--- scripts/nfs_disk_usage.sh
+++ scripts/nfs_disk_usage.sh
@@ -1,33 +1,24 @@
 #!/bin/bash
 
-runningFile=/root/metrics-running.txt
-
-mkdir -p $METRICS_FILE_PATH
-
-if [ -f $runningFile ]; then
-  exit # File exists, which means prev execution has not completed.
-else
-  if [ $1 = . ]; then
-    path=$(pwd)
-  else
-    path=$1
-  fi
-
-  # Actions
-  array=($(ls -d $path/*/))
-
-  # Set up a metric file
-  echo "# HELP nfs_disk_usage_bytes The number of bytes usage" > $runningFile
-  echo "# TYPE nfs_disk_usage_bytes gauge" >> $runningFile
-
-  for i in ${array[@]}
-  do
-    cd $i
-    record="$(du -s -b)"
-    bytes=$(echo "$record" | awk '{ print substr( $0, 1, length($0)-2 ) }')
-
-    echo "nfs_disk_usage_bytes{volume=\"$i\"} $bytes" >> $runningFile
-  done
-
-  mv $runningFile $METRICS_FILE_PATH/metrics.txt
+if [ ! -d "$1" ]; then
+  echo "No such file or directory"
+  exit
 fi
+
+# List directories
+directories=($(ls -d $1/*/))
+
+# Set up a metric file
+echo "# HELP nfs_disk_usage_bytes The number of bytes usage" > test.txt
+echo "# TYPE nfs_disk_usage_bytes gauge" >> test.txt
+
+for i in ${directories[@]}
+do
+  record="$(du -s -b $i)"
+  bytes=$(awk -F' ' '{print $1}' <<< $record)
+
+  echo "nfs_disk_usage_bytes{volume=\"$i\"} $bytes" >> test.txt
+done
+
+# Push to pushgateway
+curl --data-binary @test.txt http://<pushgateway>:9091/metrics/job/nfs_disk_usage
